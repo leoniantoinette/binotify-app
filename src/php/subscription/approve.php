e5e0b3b6708955193b2ae19d95d566cd92a2be67
Leoni <leoniantoinette12@gmail.com>
2022-12-01 17:11:33 +0700

    update db when approve or reject subscription

---
+++ src/php/subscription/approve.php
@@ -0,0 +1,15 @@
+<?php
+
+include '../../config/config.php';
+
+$creatorID = $_POST["creatorID"];
+$subscriberID = $_POST["subscriberID"];
+
+$query = "UPDATE subscription SET status = 'ACCEPTED' WHERE creator_id = " . $creatorID . " AND subscriber_id = " . $subscriberID;
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+  echo true;
+} else {
+  echo false;
+}

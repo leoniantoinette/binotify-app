19fd547a462d75183da75be39588f35407e2ca8d
Leoni <leoniantoinette12@gmail.com>
2022-12-02 14:43:29 +0700

    fix path premium song

--- src/php/song/editPremiumSong.php
+++ src/php/song/editPremiumSong.php
@@ -1,34 +1,34 @@
 <?php
-    header('Access-Control-Allow-Origin: http://localhost:3000');
-    header('Access-Control-Allow-Credentials: true');
-    ini_set('display_errors', 1); error_reporting(1);
-    function checkFile($file_name_comp, $target_dir)
-     {
-    $extension = pathinfo($file_name_comp, PATHINFO_EXTENSION);
-    $file_name = pathinfo($file_name_comp, PATHINFO_FILENAME);
-    $file_name_ori = $file_name;
-  
-    // Check if file already exists
-    $num = 1;
-    while (file_exists($target_dir . $file_name . "." . $extension)) {
-      $file_name = (string) $file_name_ori . $num;
-      $file_name_comp = $file_name . "." . $extension;
-      $num++;
-    }
-  
-    return $target_dir . $file_name_comp;  
-     }
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+ini_set('display_errors', 1);
+error_reporting(1);
+function checkFile($file_name_comp, $target_dir)
+{
+  $userID = $_POST['user_id'];
+  $extension = pathinfo($file_name_comp, PATHINFO_EXTENSION);
+  $file_name = pathinfo($file_name_comp, PATHINFO_FILENAME);
+  $file_name_ori = $file_name;
 
-    if (isset($_FILES['audiofile'])){
-        $target_dir_audio = "../../assets/PremiumSong/";
-        $path_dir_audio ="assets/PremiumSong/";
-        $audiofile_name_comp = basename($_FILES['audiofile']['name']);
+  // Check if file already exists
+  $num = 1;
+  while (file_exists($target_dir . $file_name . "." . $extension)) {
+    $file_name = (string) $file_name_ori . $num;
+    $file_name_comp = $file_name . "." . $extension;
+    $num++;
+  }
 
-        $target_audiofile = checkFile($audiofile_name_comp, $target_dir_audio);
-        $path_audiofile = checkFile($audiofile_name_comp, $path_dir_audio);
+  return $target_dir . $userID . $file_name_comp;
+}
 
-        $isAudioUploaded = move_uploaded_file($_FILES["audiofile"]["tmp_name"], $target_audiofile);
-        echo "audio is uploaded";
+if (isset($_FILES['audiofile'])) {
+  $target_dir_audio = "../../assets/PremiumSong/";
+  $path_dir_audio = "assets/PremiumSong/";
+  $audiofile_name_comp = basename($_FILES['audiofile']['name']);
 
+  $target_audiofile = checkFile($audiofile_name_comp, $target_dir_audio);
+  $path_audiofile = checkFile($audiofile_name_comp, $path_dir_audio);
 
-    }
+  $isAudioUploaded = move_uploaded_file($_FILES["audiofile"]["tmp_name"], $target_audiofile);
+  echo "audio is uploaded";
+}
